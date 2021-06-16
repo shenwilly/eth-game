0c17f5cec264a55d9960aa620d467ef730697a28
Willy <shen.willy95@gmail.com>
2021-06-16 21:34:07 +0700

    Initial commit

---
+++ test/counter.ts
@@ -0,0 +1,32 @@
+import { ethers} from "hardhat";
+import chai from "chai";
+import { Counter__factory, Counter } from "../typechain";
+
+const { expect } = chai;
+
+describe("Counter", () => {
+  let counter: Counter;
+
+  beforeEach(async () => {
+    const signers = await ethers.getSigners();
+
+    const counterFactory = (await ethers.getContractFactory(
+      "Counter",
+      signers[0]
+    )) as Counter__factory;
+    counter = await counterFactory.deploy();
+    await counter.deployed();
+    const initialCount = await counter.getCount();
+
+    expect(initialCount).to.eq(0);
+    expect(counter.address).to.properAddress;
+  });
+
+  describe("count up", async () => {
+    it("should count up", async () => {
+      await counter.countUp();
+      let count = await counter.getCount();
+      expect(count).to.eq(1);
+    });
+  });
+});
